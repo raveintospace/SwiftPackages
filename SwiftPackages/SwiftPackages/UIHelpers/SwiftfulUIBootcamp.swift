dc34cd29d5aa8fbd4ce766dab8c0868f2ebb6b35
Oriol Angelet <uriaeri@gmail.com>
2024-08-31 16:05:21 +0200

    SwiftfulUI pt1

---
+++ SwiftPackages/SwiftPackages/UIHelpers/SwiftfulUIBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftfulUIBootcamp.swift
+//  SwiftPackages
+//
+//  Created by Uri on 31/8/24.
+//
+
+import SwiftUI
+import SwiftfulUI
+
+struct SwiftfulUIBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftfulUIBootcamp()
+}
